92c1de1f01797ff1e77ad65f0eca4ab71ef74519
TitanX1503 <60981537+TitanX1503@users.noreply.github.com>
2020-04-27 10:52:42 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Display();
+        }
+    }
+}
